9df1fbd99bfd3940252323966449d781710259bc
Peter Eriksson <tuvoc1@hotmail.com>
2019-05-17 00:37:05 +0200

    current step: step 5

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = [new Phrase('Life is like a box of chocolates'), new Phrase('To be or not to be'), new Phrase("I think therefore I am"), new Phrase('You only live once'), new Phrase("I would rather die of passion than of boredom")];
+        this.activePhrase = null;
+        
+        
+    }
+    
+    
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//const phrase = new Phrase();
+//const game = new Game();
+
+//console.log(game);
+
+//const phrase = new Phrase('Life is like a box of chocolates');
+
+//console.log(`Phrase - phrase: ${phrase.phrase}`);
+//ok
+
+/*
+const gamee = new Game();
+//console.log(gamee.phrases[2]);
+gamee.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
+*/
+//ok
